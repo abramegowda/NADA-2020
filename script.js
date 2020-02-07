99da29709a7fef1ad2e9c4b152e285770d764564
Archana <archana@Tekions-MacBook-Pro-3.local>
2020-02-07 15:14:02 +0530

    minor fixes

--- script.js
+++ script.js
@@ -8,4 +8,6 @@ $(document).ready(function($) {
         $('.header-right').removeClass('menu-show').addClass('menu-hide');
         $('.hamburger-icon').show();
     });
+
+    $('.venue-container .sub-section-right').css('max-width', $('.venue-container .sub-section-right abbr').outerWidth())
 });
