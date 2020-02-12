0d59c729fa4380c2eac70b876e403d18ba50a55d
Archana <archana@Tekions-MacBook-Pro-3.local>
2020-02-12 17:25:42 +0530

    elements transition added

--- script.js
+++ script.js
@@ -9,5 +9,33 @@ $(document).ready(function($) {
         $('.hamburger-icon').show();
     });
 
-    $('.venue-container .sub-section-right').css('max-width', $('.venue-container .sub-section-right abbr').outerWidth())
+    $('.venue-container .sub-section-right').css('max-width', $('.venue-container .sub-section-right abbr').outerWidth());
+
+    $.fn.visible = function(partial) {
+        var $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    };
+
+    $(".module").each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("fadeInUp");
+        }
+    });
+
+    $(window).scroll(function(event) {
+        $(".module").each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("fadeInUp");
+            }
+        });
+    });
 });
